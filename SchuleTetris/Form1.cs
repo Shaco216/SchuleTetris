cc22aabefa879f90dd6ebc2f2013d3465ec9564b
Shaco216 <p.griffin.1911@web.de>
2022-09-14 12:25:09 +0200

    Projektdateien hinzufügen.

---
+++ SchuleTetris/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchuleTetris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this als übergabeparameter um genau diese Form1 an den ctor/konstruktor des Spielfelds zu übergeben zur Erstellung des Labels:
+            Spielfeld spielfeld = new Spielfeld(this);
+
+        }
+    }
+}
---
+++ SchuleTetris/Spielfeld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SchuleTetris
+{
+    internal class Spielfeld
+    {
+        Label testlabel;
+        int AnzReihen;
+        int BreiteSpielfeld;
+        public Spielfeld(Form Mainform)
+        {
+            testlabel = new Label();
+            testlabel.Location = new Point(20, 20);
+            testlabel.BackColor = Color.HotPink;
+            testlabel.Size = new Size(30, 20);
+            //Dadurch erhalt man beim erstellen des Objekts ein Label:
+            Mainform.Controls.Add(testlabel);
+        }
+    }
+}
